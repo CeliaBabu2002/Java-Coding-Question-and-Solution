02ff6b21d865a1990ec2c4110df260a7051025cf
celia babu <85995015+CeliaBabu2002@users.noreply.github.com>
2024-10-31 11:28:04 +0530

    new commit

---
+++ calculator.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the operation:");
+        String st = in.next();
+        System.out.println("enter the numbers:");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c;
+        switch (st) {
+            case "add": c=a+b;
+                System.out.println(c);
+                break;
+            case "subtract": c=a-b;
+                System.out.println(c);
+                break;
+            case "multiply": c=a*b;
+                System.out.println(c);
+                break;
+            case "divide": c=a/b;
+                System.out.println(c);
+                break;
+            default:System.out.println("invalid input");
+            }
+    }
+    
+}
